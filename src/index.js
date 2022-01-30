8570a54e31587cc5c00eadcb05cea774231b8355
CigdemOzturk <cigdemozturk_@hotmail.com>
2022-01-30 21:30:56 +0000

    button added using antd

--- src/index.js
+++ src/index.js
@@ -3,10 +3,20 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
+import "antd/dist/antd.css";
+import { Button } from "antd";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Button
+      className="antdbutton"
+      type="primary"
+      href="https://en.wikipedia.org/wiki/Columbidae"
+      target="_blank"
+    >
+      MORE PIGEONS ON WIKI
+    </Button>
+    <App></App>
   </React.StrictMode>,
   document.getElementById("root")
 );
