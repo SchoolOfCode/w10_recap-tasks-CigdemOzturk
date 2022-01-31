55c428265cff9192c819218ebf500f550f38e4f6
CigdemOzturk <cigdemozturk_@hotmail.com>
2022-01-31 08:39:16 +0000

    Auth0 added

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,17 @@
 import css from "./App.module.css";
 import articles from "../../libs/articles";
 import Article from "../Article";
+import LoginButton from "../LoginButton";
+import LogoutButton from "../LogoutButton";
+import Profile from "../Profile";
 function App() {
   return (
     <main className={css.App}>
+      <div className={css.buttons}>
+        <LoginButton></LoginButton> {"  "}
+        <LogoutButton></LogoutButton>
+      </div>
+      <Profile></Profile>
       <h1>WikiPigeon</h1>
       {articles.map((article, index) => {
         return <Article article={article} key={index}></Article>;
--- src/index.js
+++ src/index.js
@@ -5,19 +5,27 @@ import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
 import "antd/dist/antd.css";
 import { Button } from "antd";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Button
-      className="antdbutton"
-      type="primary"
-      href="https://en.wikipedia.org/wiki/Columbidae"
-      target="_blank"
-    >
-      MORE PIGEONS ON WIKI
-    </Button>
-    <App></App>
-  </React.StrictMode>,
+  <Auth0Provider
+    domain="dev-r998x7jx.eu.auth0.com"
+    clientId="03VxMv2wzQuEBcphhTacjXJurlSBxVwV"
+    redirectUri={window.location.origin}
+  >
+    <React.StrictMode>
+      <Button
+        className="antdbutton"
+        type="primary"
+        href="https://en.wikipedia.org/wiki/Columbidae"
+        target="_blank"
+      >
+        MORE PIGEONS ON WIKI
+      </Button>
+      <App></App>
+    </React.StrictMode>
+    ,
+  </Auth0Provider>,
   document.getElementById("root")
 );
 
